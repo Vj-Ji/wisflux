a9472036e7d3abd9ac1665c9f99ec94e594732cb
Vj <vjmaharshi2000@gmail.com>
2022-03-21 14:38:37 +0530

    Solution 1st

---
+++ index.js
@@ -0,0 +1,18 @@
+import _ from "lodash";
+
+console.log("Hello World");
+const members = [
+  { name: "Laveesh Gupta", age: 20 },
+  { name: "Yash Jangid", age: 40 },
+  { name: "Firoz Khan", age: 41 },
+  { name: "Amrit Srivastava", age: 17 },
+  { name: "Chandraprakash Sharma" },
+  { name: "Swpril Ahuja", age: 45 },
+  { name: "Yogesh Khatri", age: 51 },
+];
+
+// 1. Get array of first names of everyone
+const fname = [];
+members.map((member) => fname.push(_.split(member.name, " ")[0]));
+
+// console.log(fname);
