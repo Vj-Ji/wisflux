edb3eb00d26dafdaa7b4e44a19973dbe53f7e7b0
Vj <vjmaharshi2000@gmail.com>
2022-03-21 15:03:17 +0530

    Solution 7th

--- index.js
+++ index.js
@@ -72,4 +72,10 @@ _.map(members, (member) => {
   else groups.noage.push(member);
 });
 
-console.log(groups);
+// console.log(groups);
+
+// 7. add a new member to same members array instance at index 2
+const updatedMembers = [...members];
+updatedMembers.splice(2, 0, { name: "Ashu", age: 20 });
+
+console.log(updatedMembers);
