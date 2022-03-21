707f5839ab52f009a75863de10faad5790227d48
Vj <vjmaharshi2000@gmail.com>
2022-03-21 10:33:54 +0530

    1st solution

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+const members = [
+  { name: "Laveesh Gupta", age: 20 },
+  { name: "Yash Jangid", age: 40 },
+  { name: "Firoz Khan", age: 41 },
+  { name: "Amrit Srivastava", age: 17 },
+  { name: "Chandraprakash Sharma" },
+  { name: "Swpril Ahuja", age: 45 },
+  { name: "Yogesh Khatri", age: 51 },
+];
+
+const fname = [];
+members.map((member) => fname.push(member.name));
+console.log(fname);
